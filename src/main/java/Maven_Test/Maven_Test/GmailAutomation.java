78ce69780c44a8ae2fc4e2b6372a19d1dfa0f02f
Bhupender singh <bhupendersingh369@gmail.com>
2018-05-24 23:32:29 +0530

    thrid commit

--- src/main/java/Maven_Test/Maven_Test/GmailAutomation.java
+++ src/main/java/Maven_Test/Maven_Test/GmailAutomation.java
@@ -2,9 +2,12 @@ package Maven_Test.Maven_Test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class GmailAutomation {
-	public static void main(String[] args) {
+	
+	@Test
+	public void test(){
 		System.setProperty("webdriver.chrome.driver", "E:\\WorkSpace\\Maven_Test\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.com");
